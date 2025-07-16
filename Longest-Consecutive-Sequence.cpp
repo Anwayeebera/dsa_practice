9feda377fc7ad6a14e0a910301304a00c442a89a
Anwayee Bera <146617989+Anwayeebera@users.noreply.github.com>
2025-07-16 17:47:55 +0530

    [128] [Time Beats: 74.57%] [Memory Beats: 65.32%] - LeetPush

--- Longest-Consecutive-Sequence.cpp
+++ Longest-Consecutive-Sequence.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0)return 0;
-        sort(nums.begin(),nums.end());
-        int n= nums.size();
-        int cnt=0;
+        int n=nums.size();
+        if(n==0)return 0;
+        // sort(nums.begin(),nums.end());
+        
+        // int n= nums.size();
+        // int cnt=0;
+        // int longest=1;
+        // int lastsmall=INT_MIN;
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]-1==lastsmall){
+        //         cnt=cnt+1;
+        //         lastsmall=nums[i];
+        //     }
+        //     else if(nums[i]!=lastsmall){
+        //         cnt=1;
+        //         lastsmall=nums[i];
+        //     }
+        //     longest=max(longest,cnt);
+        // }
+        // return longest;
+
         int longest=1;
-        int lastsmall=INT_MIN;
+        unordered_set<int> st;
         for(int i=0;i<n;i++){
-            if(nums[i]-1==lastsmall){
+            st.insert(nums[i]);
+        }
+        for (auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int x=it;
+
+            
+            while(st.find(x+1)!=st.end()){
+                x=x+1;
                 cnt=cnt+1;
-                lastsmall=nums[i];
-            }
-            else if(nums[i]!=lastsmall){
-                cnt=1;
-                lastsmall=nums[i];
             }
             longest=max(longest,cnt);
         }
+        }
         return longest;
-
 
     }
 };
